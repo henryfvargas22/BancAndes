3dcf97a52847a16760e99c5a9bc1bc9737746e95
henryfvargas22 <henryfvargas22@gmail.com>
2015-09-22 16:45:14 -0500

    Warnings DAO

--- app/dao/ConsultaDAO.java
+++ app/dao/ConsultaDAO.java
@@ -4,14 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.Properties;
-
-import fachada.BancAndes;
-import vos.Usuario;
 
 public class ConsultaDAO 
 {
